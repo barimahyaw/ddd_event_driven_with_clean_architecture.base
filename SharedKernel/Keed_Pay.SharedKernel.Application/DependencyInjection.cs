2450aa57b51f9f28a1d1a1027ff35f8543143ccb
Isaac Nana Gyan Ayeh <nanabarimah22@gmail.com>
2024-03-17 15:20:25 +0000

    update

--- SharedKernel/Keed_Pay.SharedKernel.Application/DependencyInjection.cs
+++ SharedKernel/Keed_Pay.SharedKernel.Application/DependencyInjection.cs
@@ -1,6 +1,8 @@
 ﻿using FluentValidation;
 using DDD_Event_Driven_Clean_Architecture.SharedKernel.Application.Behaviors;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using MassTransit;
 
 namespace DDD_Event_Driven_Clean_Architecture.SharedKernel.Application;
 
@@ -25,6 +27,40 @@ public static class DependencyInjection
         where T : class
     {
         services.AddValidatorsFromAssembly(typeof(T).Assembly, includeInternalTypes: true);
+        return services;
+    }
+
+    public static IServiceCollection AddMassTransitRabbitMQConfig<T>(this IServiceCollection services, IConfiguration configuration)
+        where T : class
+    {
+        services.AddMassTransit(config =>
+        {
+            config.AddConsumersFromNamespaceContaining<T>();
+
+            config.UsingRabbitMq((ctx, cfg) =>
+            {
+                var uri = new Uri(Environment.GetEnvironmentVariable("SERVICE_BUS_URI")
+                    ?? configuration["SERVICE_BUS_URI"]
+                    ?? throw new ArgumentNullException("SERVICE_BUS_URI"));
+
+                var userName = Environment.GetEnvironmentVariable("SERVICE_BUS_USER_NAME")
+                    ?? configuration["SERVICE_BUS_USER_NAME"]
+                    ?? throw new ArgumentNullException("SERVICE_BUS_USER_NAME");
+
+                var password = Environment.GetEnvironmentVariable("SERVICE_BUS_PASSWORD")
+                    ?? configuration["SERVICE_BUS_PASSWORD"]
+                    ?? throw new ArgumentNullException("SERVICE_BUS_PASSWORD");
+
+                cfg.Host(uri, host =>
+                {
+                    host.Username(userName);
+                    host.Password(password);
+                });
+
+                cfg.ConfigureEndpoints(ctx);
+            });
+        });
+
         return services;
     }
 }
